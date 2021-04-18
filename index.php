8aaf6a050516b71a0fc5b4c763a355ad295df34c
root <root@tangled.ae>
2021-04-18 07:52:33 +0000

    Added profile pictures

--- index.php
+++ index.php
@@ -10,12 +10,59 @@ $pass = mysqli_real_escape_string($link, $_REQUEST['pass']);
 $user = mysqli_real_escape_string($link, $_REQUEST['user']);
 $age = mysqli_real_escape_string($link, $_REQUEST['dob']);
 $ssn = mysqli_real_escape_string($link, $_REQUEST['ssn']);
-$pic = mysqli_real_escape_string($link, $_REQUEST['pic']);
+$pic = mysqli_real_escape_string($link, $_REQUEST['the_file']);
 
 $sql = "INSERT INTO sustenance (Name, Email, Username, Password, dob, ssn, pic) VALUES ('$name', '$email', '$user', '$pass', '$age', '$ssn', '$pic');";
 if(isset($_POST['submit'])) {
 if(mysqli_query($link, $sql))
 {
+	$currentDirectory = getcwd();
+    $uploadDirectory = "/images/";
+
+    $errors = [];
+
+    $fileExtensionsAllowed = ['jpeg','jpg','png']; 
+
+    $fileName = $_FILES['the_file']['name'];
+    $fileSize = $_FILES['the_file']['size'];
+    $fileTmpName  = $_FILES['the_file']['tmp_name'];
+    $fileType = $_FILES['the_file']['type'];
+    $fileExtension = strtolower(end(explode('.',$fileName)));
+
+    $uploadPath = $currentDirectory . $uploadDirectory . basename($fileName); 
+
+    if (isset($_POST['submit'])) {
+
+      if (! in_array($fileExtension,$fileExtensionsAllowed)) {
+        $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+      }
+
+      if ($fileSize > 4000000) {
+        $errors[] = "File exceeds maximum size (4MB)";
+      }
+
+      if (empty($errors)) {
+        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+        if ($didUpload) {
+          echo "The file " . basename($fileName) . " has been uploaded";
+        $sql2 = "UPDATE sustenance set pic = '$fileName' where Username = '$user';";
+        if(mysqli_query($link, $sql2)) {
+        	echo "done2";
+        }
+        else {
+        	echo "noo";
+        }
+        } else {
+          echo "An error occurred. Please contact the administrator.";
+        }
+      } else {
+        foreach ($errors as $error) {
+          echo $error . "These are the errors" . "\n";
+        }
+      }
+
+    }
     echo "done";
 } 
 else
@@ -55,7 +102,7 @@ mysqli_close($link);
                 <button type="submit" class="submit-btn">Log In</button>
             </form>
 
-            <form id="register" method = "post" action = "index.php" class="input-group">
+            <form id="register" method = "post" action = "index.php" class="input-group" enctype="multipart/form-data">
                 <input type="text" class="input-field" placeholder="Name" name = "name" autocomplete="name" required />
                 <input type="email" class="input-field" placeholder="Enter Email" name = "email" required />
             	<input type="text" class="input-field" placeholder="Username" name = "user" autocomplete="username" required />
@@ -63,7 +110,7 @@ mysqli_close($link);
             	<input type="checkbox" class="check-box" onclick="showPass()"/>Show Password
             	<input type="date" class="input-field" name = "dob" autocomplete="dob" required />
             	<input type="text" class="input-field" placeholder="Social Security Number" name = "ssn" autocomplete="ssn" required />
-            	<input type = "file" name = "pic" placeholder = "pic">
+            	<input type = "file" placeholder = "pic" name="the_file" id="fileToUpload">
                 <button type="submit" class="submit-btn" name = "submit">Register</button>
             </form>
         </div>
