7cad8492053c403e029c8f9dc034868fcd55f832
Tiffany Trinh <acetiff@mit.edu>
2021-04-18 09:22:28 -0400

    fixed feat name in upload

--- feed.php
+++ feed.php
@@ -150,7 +150,7 @@ echo '
         <form method="post" enctype="multipart/form-data" class="fileUpload">
         Upload a Feat:
         <input type="file" name="the_file" id="fileToUpload">
-        <input type="submit" name="submit2" value="Upload my fleet!">
+        <input type="submit" name="submit2" value="Upload my feat!">
     	</form>
         <div id="feed">';
 $feats = "SELECT * FROM feats;";
